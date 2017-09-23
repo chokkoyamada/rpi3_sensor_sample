f6358219a62614a93ec16f24bd72925131942c92
Naoyuki Yamada <yamada@kirishikistudios.com>
2017-09-23 16:34:56 +0900

    Create sensor.py

---
+++ sensor.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import time,os
+import RPi.GPIO as GPIO
+
+INTAVAL = 3
+SLEEPTIME = 5
+SENSOR_PIN = 18
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(SENSOR_PIN, GPIO.IN)
+GPIO.setup(22, GPIO.OUT)
+
+st = time.time()-INTAVAL
+
+try:
+  for i in range(30):
+    print GPIO.input(SENSOR_PIN)
+    if(GPIO.input(SENSOR_PIN) == GPIO.HIGH) and (st + INTAVAL < time.time()):
+      st = time.time()
+      print("人を感知しました")
+      os.system('sh post.sh Detected_something! ')
+      GPIO.output(22, True)
+    else:
+      GPIO.output(22, False)
+    time.sleep(SLEEPTIME)
+
+except KeyboardInterrupt:
+  print "KeyboardInterrupt"
+
+except:
+  print "Other error or exception occurred!"
+
+finally:
+  GPIO.cleanup()
